6a70d44e3af923c6eccc387efb58c5b72d74d8ca
Michal Konecny <michalkonec666@gmail.com>
2018-11-03 17:46:56 +0100

    Use PHP7.2

--- build.sh
+++ build.sh
@@ -3,8 +3,11 @@
 IMAGE_NAME="centos-owncloud:latest"
 
 ctr=$(buildah from centos)
-buildah run $ctr -- yum install -y centos-release-scl epel-release
-buildah run $ctr -- yum install -y nginx rh-php70 rh-php70-php rh-php70-php-gd rh-php70-php-mbstring rh-php70-php-mysqlnd
+buildah run $ctr -- yum install -y centos-release-scl epel-release yum-utils
+buildah run $ctr -- yum install -y https://dl.fedoraproject.org/pub/epel/epel-release-latest-7.noarch.rpm
+buildah run $ctr -- yum install -y http://rpms.remirepo.net/enterprise/remi-release-7.rpm
+buildah run $ctr -- yum-config-manager --enable remi-php72
+buildah run $ctr -- yum install -y nginx
 buildah run $ctr -- yum install -y php php-mysql php-pecl-zip php-xml php-mbstring php-gd php-fpm php-intl
 buildah run $ctr -- yum install -y wget
 
@@ -37,8 +40,14 @@ buildah run $ctr -- yum -y install owncloud-files
 # testing -- use external DB ideal
 #buildah run $ctr -- yum -y install wget mariadb-server mariadb
 
-buildah run $ctr -- systemctl enable nginx php-fpm
-buildah run $ctr -- systemctl restart php-fpm
+# This needs to be done - because php-fpm is creating pid file in this location
+# Why this isn't done by rpm package is mystery
+buildah run $ctr mkdir -p /run/php-fpm
+buildah run $ctr php-fpm
+buildah run $ctr nginx
+
+#buildah run $ctr -- systemctl enable nginx php-fpm
+#buildah run $ctr -- systemctl restart php-fpm
 
 buildah config --author 'Jiri Konecny' $ctr
 buildah config --port 8080 $ctr
