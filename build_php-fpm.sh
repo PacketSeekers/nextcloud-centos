0a2657bd8b3682f881fe75967c506725a9ca66af
Jiri Konecny <jkonecny@redhat.com>
2018-11-28 23:35:21 +0100

    Continue work on php-fpm script
    Remove unused parts and add updates.

--- build_php-fpm.sh
+++ build_php-fpm.sh
@@ -3,16 +3,9 @@
 IMAGE_NAME="centos-php-fpm:latest"
 
 ctr=$(buildah from centos)
+buildah run $ctr -- yum update -y
 buildah run $ctr -- yum install -y centos-release-scl epel-release yum-utils
 buildah run $ctr -- yum install -y rh-php70 rh-php70-php rh-php70-php-fpm
-
-# FIXME: probably not required in container
-#buildah run $ctr -- semanage fcontext -a -t httpd_sys_rw_content_t '/var/www/html/owncloud/data'
-#buildah run $ctr -- restorecon '/var/www/html/owncloud/data'
-#buildah run $ctr -- semanage fcontext -a -t httpd_sys_rw_content_t '/var/www/html/owncloud/config'
-#buildah run $ctr -- restorecon '/var/www/html/owncloud/config'
-#buildah run $ctr -- semanage fcontext -a -t httpd_sys_rw_content_t '/var/www/html/owncloud/apps'
-#buildah run $ctr -- restorecon '/var/www/html/owncloud/apps'
 
 # FIXME: is this still required?
 # This needs to be done - because php-fpm is creating pid file in this location
